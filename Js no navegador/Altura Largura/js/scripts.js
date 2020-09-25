a7827179c0305547e1eb87e66b99df6be2fd16b7
matheusfalavigna <matheusfalavigna@gmail.com>
2020-09-25 16:52:06 -0300

    Exercises JavaScript no Navegador - Altura e largura

---
+++ Js no navegador/Altura Largura/js/scripts.js
@@ -0,0 +1,7 @@
+let elemento = document.querySelector("#Titulo-principal");
+
+console.log("Largura: " + elemento.offsetWidth);
+console.log("Altura: " + elemento.offsetHeight);
+
+console.log("Largura: " + elemento.clientWidth);
+console.log("Altura: " + elemento.clientHeight);
