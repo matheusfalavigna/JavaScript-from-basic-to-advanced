e6025b26b5b35e0fbd42877d8166de05ab13ce76
matheusfalavigna <matheusfalavigna@gmail.com>
2020-10-16 08:08:07 -0300

    Exercises Eventos com JS - Evento foco

---
+++ Eventos com Js/Evento foco/js/script.js
@@ -0,0 +1,9 @@
+let input = document.querySelector("#input1");
+
+input.addEventListener("focus", () => {
+    console.log("Entrou no input");
+});
+
+input.addEventListener("blur", () => {
+    console.log("Saiu no input");
+});
